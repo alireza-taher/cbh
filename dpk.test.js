26eee2f67dff825afb875ae9ea47c187a83e4491
alireza <alireza.taher@icloud.com>
2022-09-27 19:08:36 +0400

    Add test for output when input is provided

--- dpk.test.js
+++ dpk.test.js
@@ -5,4 +5,9 @@ describe("deterministicPartitionKey", () => {
     const trivialKey = deterministicPartitionKey();
     expect(trivialKey).toBe("0");
   });
+
+  it("Returns non-'0' literal when given an input", () => {
+    const trivialKey = deterministicPartitionKey('Foo');
+    expect(trivialKey).not.toBe("0");
+  });
 });
