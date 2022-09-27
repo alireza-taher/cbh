b810731cda4ea6ccd4569c0562a0a3f6adf53a32
alireza <alireza.taher@icloud.com>
2022-09-27 19:27:07 +0400

    Add more tests

--- dpk.test.js
+++ dpk.test.js
@@ -10,4 +10,16 @@ describe("deterministicPartitionKey", () => {
     const trivialKey = deterministicPartitionKey('Foo');
     expect(trivialKey).not.toBe("0");
   });
+
+  it("Return different output for different input", () => {
+    const trivialKey1 = deterministicPartitionKey('Foo');
+    const trivialKey2 = deterministicPartitionKey('Bar');
+    expect(trivialKey1).not.toBe(trivialKey2);
+  })
+
+  it("Return consistent output for the same input", () => {
+    const trivialKey1 = deterministicPartitionKey('Foo');
+    const trivialKey2 = deterministicPartitionKey('Foo');
+    expect(trivialKey1).toBe(trivialKey2);
+  })
 });
