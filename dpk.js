9f175467653e9bdb13fc4df524f8ae277f82856b
alireza <alireza.taher@icloud.com>
2022-09-27 19:08:04 +0400

    Remove second if-else with a single if

--- dpk.js
+++ dpk.js
@@ -13,14 +13,13 @@ exports.deterministicPartitionKey = (event) => {
       candidate = crypto.createHash("sha3-512").update(data).digest("hex");
     }
   }
-
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
+  
+  if (typeof candidate !== "string") {
+    candidate = JSON.stringify(candidate);
   }
+  
+  if(!candidate) candidate = TRIVIAL_PARTITION_KEY;
+  
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
     candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
   }
