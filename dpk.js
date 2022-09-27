05ff57f2027f7ff7aaa7822f79b529e6420bd796
Alireza <topdevelopers.org@gmail.com>
2022-09-27 19:22:06 +0400

    Add format_candidate function
    Add format_candidate function and replace the corresponding part in deterministicPartitionKey method with a call.

--- dpk.js
+++ dpk.js
@@ -1,21 +1,8 @@
 const crypto = require("crypto");
 
 exports.deterministicPartitionKey = (event) => {
-  const TRIVIAL_PARTITION_KEY = "0";
-  const MAX_PARTITION_KEY_LENGTH = 256;
-  
   let candidate = get_event_candidate(event);
-  
-  if (typeof candidate !== "string") {
-    candidate = JSON.stringify(candidate);
-  }
-  
-  if(!candidate) candidate = TRIVIAL_PARTITION_KEY;
-  
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
-  }
-  return candidate;
+  return format_candidate(candidate);
 };
 
 function get_event_candidate(event) {
@@ -25,4 +12,21 @@ function get_event_candidate(event) {
 
   const data = JSON.stringify(event);
   return crypto.createHash("sha3-512").update(data).digest("hex");
+}
+
+function format_candidate(candidate) {
+  const TRIVIAL_PARTITION_KEY = "0";
+  const MAX_PARTITION_KEY_LENGTH = 256;
+
+  if(!candidate) candidate = TRIVIAL_PARTITION_KEY;
+
+  if (typeof candidate !== "string") {
+    candidate = JSON.stringify(candidate);
+  }
+
+  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
+    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
+  }
+
+  return candidate;
 }
